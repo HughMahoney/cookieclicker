d7a96bd17d3c3f5910bb7deea3229b63ba7f25ad
HughMahoney <47137792+HughMahoney@users.noreply.github.com>
2019-03-09 16:06:12 +1030

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let counts = 0
+
+
+function count() {
+  counts++;
+let span1 = document.querySelector('i')
+span1.textContent = counts
+let img = document.querySelector('img')
+img.src = 'cookie.png'
+  
